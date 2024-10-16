365277fbc1a4be63ebd3d511dbffba7e68df696e
Akash Arumugam Venkatachalapathy <144757714+AkashArumugam6@users.noreply.github.com>
2024-10-16 17:22:52 -0400

    fmt check3

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block = varr.vpc_cidr
+          resource "aws_vpc" "myvpc" {
+  cidr_block = var.vpc_cidr
 
 
 
-  tags = {
+      tags = {
     Name = "My VPC"
   }
 }
