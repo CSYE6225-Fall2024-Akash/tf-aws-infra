1c09257cce67e350f333bd1f417c652b632a1b66
AkashArumugam6 <venkatachalapathy.a@northeastern.edu>
2024-10-23 20:19:58 -0400

    Updated ec2, security group and variables terraform config

--- ec2.tf
+++ ec2.tf
@@ -13,6 +13,27 @@ resource "aws_instance" "web_app" {
 
   disable_api_termination = false
 
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+echo "DB_HOST=${aws_db_instance.mydb.address}" >> /opt/webapp/webapp/.env
+echo "DB_USER=csye6225" >> /opt/webapp/webapp/.env
+echo "DB_PASSWORD=${var.db_password}" >> /opt/webapp/webapp/.env
+echo "DB_NAME=csye6225" >> /opt/webapp/webapp/.env
+echo "DB_PORT=${var.db_port}" >> /opt/webapp/webapp/.env
+echo "PORT=3000" >> /opt/webapp/webapp/.env
+
+sudo chown -R csye6225:csye6225 /opt/webapp/webapp
+
+# Set the correct permissions (600) for the .env file
+chmod 600 /opt/webapp/webapp/.env
+
+# Navigate to the application directory (ensure your app is here)
+cd /opt/webapp/webapp
+sudo systemctl start app.service 
+EOF
+  )
+
   tags = {
     Name        = "${var.environment}--app-server"
     Environment = var.environment
