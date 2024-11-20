8815112d45a96c749209af98f7516cdb034a5958
AkashArumugam6 <venkatachalapathy.a@northeastern.edu>
2024-11-20 18:33:37 -0500

    Update lb security group

--- lb_security_group.tf
+++ lb_security_group.tf
@@ -10,6 +10,7 @@ resource "aws_security_group" "load_balancer" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   # Allow HTTPS traffic
@@ -19,6 +20,7 @@ resource "aws_security_group" "load_balancer" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   # Allow all outbound traffic
@@ -27,6 +29,7 @@ resource "aws_security_group" "load_balancer" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
